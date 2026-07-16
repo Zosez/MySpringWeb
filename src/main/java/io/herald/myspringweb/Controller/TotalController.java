d2bb71a1150d73f7662f3ef76aa37175a3b39f20
Zosez <2kashyap.adhikari@gmail.com>
2026-07-16 13:56:05 +0545

    Initial commit

---
+++ src/main/java/io/herald/myspringweb/Controller/TotalController.java
@@ -0,0 +1,22 @@
+package io.herald.myspringweb.Controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+//Controller handles http requests (Get, Post, Put, Delete)
+@Controller
+public class TotalController {
+
+
+    @GetMapping("/")
+    public String firstPage(){
+        return "index";
+    }
+
+
+
+
+
+
+}
---
+++ src/test/java/io/herald/myspringweb/MySpringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package io.herald.myspringweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
