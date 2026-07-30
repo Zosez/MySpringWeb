05788b825e9a1b045b129cb0d0233ce3541f9c50
Zosez <2kashyap.adhikari@gmail.com>
2026-07-30 14:17:16 +0545

    Complete CRUD, styling login, signup page

--- src/main/java/io/herald/myspringweb/Controller/GalleryController.java
+++ src/main/java/io/herald/myspringweb/Controller/GalleryController.java
@@ -1,14 +1,26 @@
 package io.herald.myspringweb.Controller;
 
 
+import io.herald.myspringweb.Model.ImageTable;
+import io.herald.myspringweb.Repository.ImageRepository;
 import jakarta.servlet.http.HttpSession;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Base64;
 
 @Controller
 public class GalleryController {
+
+    @Autowired
+    private ImageRepository imgRepo;
 
     @GetMapping("/gallery")
     public String galleryGet(HttpServletRequest request, Model m){
@@ -23,7 +35,27 @@ public class GalleryController {
 
         return "galleryPage";
 
+    }
+    @PostMapping("/galleryPost")
+    public String galleryPost(@RequestParam("imgFile")MultipartFile imgFile, HttpSession session){
+        try{
+            byte[] imgBytes = imgFile.getBytes();
+            String imgString = Base64.getEncoder().encodeToString(imgBytes);
+            ImageTable img = new ImageTable();
+
+            img.setImage(imgString);
+
+            imgRepo.save(img);
 
 
+        }catch(IOException e){
+
+            e.printStackTrace();
+
+        }
+
+        session.setAttribute("totalImages",imgRepo.findAll());
+
+        return "galleryPage";
     }
 }
